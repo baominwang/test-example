25474922a30c2fb49a6f17e45f8d7bbbec87b3b7
baominwang <greencacti@163.com>
2022-06-30 11:16:26 +0800

    Add parameter support

--- src/test/java/com/example/Test1.java
+++ src/test/java/com/example/Test1.java
@@ -1,16 +1,18 @@
 package com.example;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
-    @Test(groups = { "functest", "checkintest" })
+    @Test(groups = { "functest", "checkintest", "broken" })
     public void testMethod1() {
         System.out.println("testMethod1");
     }
 
+    @Parameters({"firstname", "lastname"})
     @Test(groups = {"functest", "checkintest"} )
-    public void testMethod2() {
-        System.out.println("testMethod2");
+    public void testMethod2(String firstname, String lastname) {
+        System.out.println("testMethod2: Hello " + firstname + ", " + lastname);
     }
 
     @Test(groups = { "functest" })
