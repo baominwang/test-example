9cb2a4bf1f5953662270d84280a5bffa50012953
baominwang <greencacti@163.com>
2022-06-30 11:21:30 +0800

    support dataprovider

--- src/test/java/com/example/Test1.java
+++ src/test/java/com/example/Test1.java
@@ -1,9 +1,19 @@
 package com.example;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
+    @DataProvider(name = "dp1")
+    public Object[][] dp1() {
+        return new Object[][] {
+                {"Benjamin", "Wang"},
+                {"Haitao", "Cao"},
+                {"Huaiyu", "Wang"}
+        };
+    }
+
     @Test(groups = { "functest", "checkintest", "broken" })
     public void testMethod1() {
         System.out.println("testMethod1");
@@ -15,9 +25,9 @@ public class Test1 {
         System.out.println("testMethod2: Hello " + firstname + ", " + lastname);
     }
 
-    @Test(groups = { "functest" })
-    public void testMethod3() {
-        System.out.println("testMethod3");
+    @Test(groups = { "functest" }, dataProvider = "dp1")
+    public void testMethod3(String firstname, String lastname) {
+        System.out.println("testMethod3: Hello " + firstname + ", " + lastname);
     }
 }
 
